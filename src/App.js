d7fd8467608d41b589f7097176f5bd9b13e59595
mccs0064 <stevenmucci91@gmail.com>
2018-12-17 14:10:12 -0500

    change name

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello BOB
+            This is from the feature branch 
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
